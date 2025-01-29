4ca45ba0d5240589f2c52180d7c659bce413dffb
Jonathan K. <noudas1@gmail.com>
2025-01-29 20:52:35 -0300

    added backend of the system

--- FrontEnd/src/api/apiClient.ts
+++ FrontEnd/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://di-final-project-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
